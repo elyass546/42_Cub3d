e7c188728484f94c0ff939e0e66ae6f56c63824b
Mohamed Korchi <korchimd@gmail.com>
2022-12-10 12:46:04 +0100

    double free ?

--- mandatory/raycasting/free.c
+++ mandatory/raycasting/free.c
@@ -6,7 +6,7 @@
 /*   By: mkorchi <mkorchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 21:53:56 by mkorchi           #+#    #+#             */
-/*   Updated: 2022/12/10 11:14:06 by mkorchi          ###   ########.fr       */
+/*   Updated: 2022/12/10 12:45:46 by mkorchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,13 @@ void	free_exit(t_data *data, int status)
 {
 	if (status)
 		ft_putstr_fd("ERROR\n", 2);
-	(void) data;
-	// if (data->mlx)
-	// {
-	// 	if (data->img.img)
-	// 		mlx_destroy_image(data->mlx, data->img.img);
-	// 	mlx_destroy_window(data->mlx, data->win);
-	// 	destroy_textures(data);
-	// 	free(data->mlx);
-	// }
-	// free(data);
+	if (data->mlx)
+	{
+		if (data->img.img)
+			mlx_destroy_image(data->mlx, data->img.img);
+		mlx_destroy_window(data->mlx, data->win);
+		free(data->mlx);
+	}
+	free(data);
 	exit(status);
-}
-
-void	destroy_textures(t_data *data)
-{
-	if (data->pars->north)
-		mlx_destroy_image(data->mlx, data->pars->north);
-	if (data->pars->west)
-		mlx_destroy_image(data->mlx, data->pars->west);
-	if (data->pars->south)
-		mlx_destroy_image(data->mlx, data->pars->south);
-	if (data->pars->east)
-		mlx_destroy_image(data->mlx, data->pars->east);
 }
